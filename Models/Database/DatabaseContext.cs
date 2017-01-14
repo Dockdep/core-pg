4d18b159db03919fa7c1afae84aa860781fe7731
Dockdep <dockdep@bigmir.net>
2017-01-14 19:03:08 +0200

    add postgres connection

---
+++ Models/Database/DatabaseContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Models.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+    }
+}
